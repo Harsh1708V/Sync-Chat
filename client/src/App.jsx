492f41ef4044924dc7e8d9aafa8b8b6f50685b4e
Harsh Waibhav <harshwaibhav69@gmail.com>
2024-08-25 02:38:14 +0530

    UI design Complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./pages/auth";
+import Chat from "./pages/chat";
+import Profile from "./pages/profile";
 
-function App() {
+
+const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
